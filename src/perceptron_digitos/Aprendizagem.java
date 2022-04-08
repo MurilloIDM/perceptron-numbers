efee29d8e712084cf1d497cfb20466b314a74702
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-04-07 22:21:05 -0300

    New: Estruturação para 4 perceptrons

---
+++ src/perceptron_digitos/Aprendizagem.java
@@ -0,0 +1,115 @@
+package perceptron_digitos;
+
+/**
+ *
+ * @author Clarimundo
+ */
+public class Aprendizagem {
+
+    private double x[][] = {
+        {1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1},
+        {1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1},
+        {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+        {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+        {1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1},
+        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+        {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1}
+    };
+
+    private double weight[][] = new double[16][4];
+    private double target[][] = {
+        {1, 1, 1, 1},
+        {1, 1, 1, -1},
+        {1, 1, -1, 1},
+        {1, 1, -1, -1},
+        {1, -1, 1, 1},
+        {1, -1, 1, -1},
+        {1, -1, -1, 1},
+        {1, -1, -1, -1},
+        {-1, 1, 1, 1},
+        {-1, 1, 1, -1}
+    };
+    private int epocas;
+
+    public Aprendizagem() {
+        epocas = 0;
+    }
+
+    public double[][] getw() {
+        return weight;
+    }
+
+    public double[][] gett() {
+        return target;
+    }
+
+    public int getepocas() {
+        return epocas;
+    }
+
+    public double somatorio(int line, int perceptron) {
+        double yent = 0;
+        for (int column = 0; column < 16; column++) {
+            yent = yent + x[line][column] * weight[line][perceptron];
+        }
+        return yent;
+    }
+
+    public double saida(double yent, double limiar) {
+        double f;
+
+        if (yent > limiar) {
+            f = 1;
+        } else if (yent < -limiar) {
+            f = -1;
+        } else {
+            f = 0;
+        }
+        return f;
+    }
+
+    public void atualiza(double alfa, double fyent[][], int perceptron) {
+        for (int line = 0; line < 10; line++) {
+            for (int column = 0; column < 16; column++) {
+                weight[line][column] = weight[line][column] + alfa * (target[line][perceptron] - fyent[line][perceptron]) * x[line][column];
+            }
+        }
+    }
+
+    public void algoritmo(double alfa, double limiar) {
+        double yent;
+        double fyent[][] = new double[10][4];
+        boolean mudou;
+
+        // Zerar os pesos
+        for (int line = 0; line < 16; line++) {
+            for (int column = 0; column < 4; column++) {
+                weight[line][column] = 0;
+            }
+        }
+
+        // Irá percorrer cada uma das linhas da matriz 
+        for (int perceptron = 0; perceptron < 4; perceptron++) {
+            do {
+                mudou = false;
+
+                for (int line = 0; line < 10; line++) {
+                    // Calcular o yent referente aquela linha
+                    yent = somatorio(line, perceptron);
+                    fyent[line][perceptron] = saida(yent, limiar);
+
+                    if (fyent[line][perceptron] != target[line][perceptron]) {
+                        mudou = true;
+                    }
+                }
+                if (mudou == true) {
+                    atualiza(alfa, fyent, perceptron);
+                }
+                epocas++;
+            } while (mudou == true);
+        }
+    }
+}
---
+++ src/perceptron_digitos/Validacao.java
@@ -0,0 +1,67 @@
+package perceptron_digitos;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Clarimundo
+ */
+public class Validacao {
+
+    public Validacao() {
+
+    }
+
+    public double somatorio(int mat[][], double w[][], int perceptron) {
+
+        double yent = 0;
+        double entrada[] = new double[16];
+        int l = 1;
+        entrada[0] = 1;
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                entrada[l] = mat[i][j];
+                l++;
+            }
+        }
+        
+        for (int j = 1; j < 16; j++) {
+            yent = yent + entrada[j] * w[j][perceptron];
+        }
+        
+        return yent;
+    }
+
+    public double saida(double yent, double limiar) {
+        double f;
+
+        if (yent > limiar) {
+            f = 1;
+        } else if (yent < -limiar) {
+            f = -1;
+        } else {
+            f = 0;
+        }
+        return f;
+    }
+
+    public String teste(int mat[][], double w[][], double t[][], double limiar) {
+        double fyent[] = new double[4];
+        
+        for (int perceptron = 0; perceptron < 4; perceptron++) {
+            double yent = somatorio(mat, w, perceptron);
+            fyent[perceptron] = saida(yent, limiar);
+        }
+        
+        if (Arrays.equals(fyent, t[0])) {
+            return "0";
+        } else if (Arrays.equals(fyent, t[1])) {
+            return "1";
+        } else {
+            return "?";
+        }
+
+    }
+    
+}
